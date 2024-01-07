cd9cb51d3b0be707fe31596ac8d7ce8a23cbf80f
JIBIN N <113666793+jibinntcr@users.noreply.github.com>
2024-01-07 17:43:39 +0530

    Create Linkedlist_midpoint.kt

---
+++ Linkedlist_midpoint.kt
@@ -0,0 +1,21 @@
+package com.igorwojda.linkedlist.singly.midpoint
+
+import com.igorwojda.linkedlist.singly.base.Solution1.Node
+import com.igorwojda.linkedlist.singly.base.Solution1.SinglyLinkedList
+
+class Solution1 {
+    companion object {
+        // Function to find the midpoint of a singly linked list
+        fun midpoint(list: SinglyLinkedList<Char>): Node<Char>? {
+            var slowPointer = list.first
+            var fastPointer = list.first
+
+            while (fastPointer?.next != null && fastPointer.next?.next != null) {
+                slowPointer = slowPointer?.next
+                fastPointer = fastPointer.next?.next
+            }
+
+            return slowPointer
+        }
+    }
+}
